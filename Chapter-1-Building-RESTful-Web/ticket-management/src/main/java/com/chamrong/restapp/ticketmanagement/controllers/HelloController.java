b542b83311f63d4fd114d41e3b32ea411b347c7a
chamrongthordev <chamrongthor.dev@gmail.com>
2021-11-28 17:46:20 +0700

    config port

--- Chapter-1-Building-RESTful-Web/ticket-management/src/main/java/com/chamrong/restapp/ticketmanagement/controllers/HelloController.java
+++ Chapter-1-Building-RESTful-Web/ticket-management/src/main/java/com/chamrong/restapp/ticketmanagement/controllers/HelloController.java
@@ -1,2 +1,14 @@
-package com.chamrong.restapp.ticketmanagement.controllers;public class HelloController {
+package com.chamrong.restapp.ticketmanagement.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class HelloController {
+  @GetMapping
+  public String sayLam() {
+    return "Lam";
+  }
 }
