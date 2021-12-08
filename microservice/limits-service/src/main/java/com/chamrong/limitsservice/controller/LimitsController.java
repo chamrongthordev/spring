10ce35f5f083770133e2887f6a569b1e79e01522
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-08 23:42:29 +0700

    Create hard cord "LimitsService"

---
+++ microservice/limits-service/src/main/java/com/chamrong/limitsservice/controller/LimitsController.java
@@ -0,0 +1,13 @@
+package com.chamrong.limitsservice.controller;
+
+import com.chamrong.limitsservice.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+  @GetMapping("/limits")
+  public Limits retrieveLimits() {
+    return new Limits(1, 1000);
+  }
+}
