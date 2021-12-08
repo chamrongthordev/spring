461c06f75ef3361138ef14d08e514c51953c1f89
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-08 23:55:31 +0700

    Config "LimitsService" get property from Application.properties

--- microservice/limits-service/src/main/java/com/chamrong/limitsservice/controller/LimitsController.java
+++ microservice/limits-service/src/main/java/com/chamrong/limitsservice/controller/LimitsController.java
@@ -1,13 +1,20 @@
 package com.chamrong.limitsservice.controller;
 
 import com.chamrong.limitsservice.bean.Limits;
+import com.chamrong.limitsservice.configuration.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
+  private final Configuration configuration;
+
+  public LimitsController(Configuration configuration) {
+    this.configuration = configuration;
+  }
+
   @GetMapping("/limits")
   public Limits retrieveLimits() {
-    return new Limits(1, 1000);
+    return new Limits(configuration.getMinimum(), configuration.getMaximum());
   }
 }
