7a0c5a46a5978fa5c3ea341aa1f8ea9944a8870e
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-09 18:09:57 +0700

    Set dynamic port

---
+++ microservice/currency-exchange-service/src/main/java/com/chamrong/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,29 @@
+package com.chamrong.currencyexchangeservice.controller;
+
+import com.chamrong.currencyexchangeservice.model.CurrencyExchange;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+  private final Environment environment;
+
+  public CurrencyExchangeController(Environment environment) {
+    this.environment = environment;
+  }
+
+  @GetMapping("/currency-exchange/from/{from}/to/{to}")
+  public CurrencyExchange retreiveExchangeValue(
+      @PathVariable String from,
+      @PathVariable String to
+  ) {
+    CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+    String port = this.environment.getProperty("local.server.port");
+    currencyExchange.setEnvironment(port);
+    return currencyExchange;
+  }
+}
